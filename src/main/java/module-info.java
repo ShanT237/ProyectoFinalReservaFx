362450b9fe86adb513ada6400d8d9ff146219ede
ShanT237 <santiago.rodriguezt@uqvirtual.edu.co>
2025-04-15 23:07:25 -0500

    CODEV(FIRST STEP)

---
+++ src/main/java/module-info.java
@@ -0,0 +1,8 @@
+module co.edu.uniquindio.proyectofinalhotelfx {
+    requires javafx.controls;
+    requires javafx.fxml;
+
+
+    opens co.edu.uniquindio.proyectofinalhotelfx to javafx.fxml;
+    exports co.edu.uniquindio.proyectofinalhotelfx;
+}
