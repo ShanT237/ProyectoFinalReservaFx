3d1664b1a9a52e8307bcd25ca25dd485ea5b2ef8
Yesuu45 <yesuue.arost@uqvirtual.edu.co>
2025-05-04 15:08:47 -0500

    3.0

--- src/main/java/co/edu/uniquindio/proyectofinalhotelfx/Modelo/Entidades/Factura.java
+++ src/main/java/co/edu/uniquindio/proyectofinalhotelfx/Modelo/Entidades/Factura.java
@@ -1,4 +1,20 @@
 package co.edu.uniquindio.proyectofinalhotelfx.Modelo.Entidades;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.time.LocalDate;
+import java.util.UUID;
+@Getter
+@Setter
+@Builder
+@AllArgsConstructor
+
 public class Factura {
+    private UUID id;
+    private double subtotal;
+    private double total;
+    private LocalDate fecha;
 }
--- src/main/java/co/edu/uniquindio/proyectofinalhotelfx/Modelo/Entidades/Hotel.java
+++ src/main/java/co/edu/uniquindio/proyectofinalhotelfx/Modelo/Entidades/Hotel.java
@@ -1,4 +1,12 @@
 package co.edu.uniquindio.proyectofinalhotelfx.Modelo.Entidades;
 
+import lombok.Builder;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@Builder
+
 public class Hotel extends Alojamiento{
 }
