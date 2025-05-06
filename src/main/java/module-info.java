078e08ee9abbf5cb7e82c4281222f2b4f3d8b3bd
Yesuu45 <yesuue.arost@uqvirtual.edu.co>
2025-05-06 15:16:11 -0500

    Registro de usuario fxml

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -4,9 +4,13 @@ module co.edu.uniquindio.proyectofinalhotelfx {
     requires static lombok;
     requires java.desktop;
 
-
-    opens co.edu.uniquindio.proyectofinalhotelfx to javafx.fxml;
-    exports co.edu.uniquindio.proyectofinalhotelfx;
-    exports co.edu.uniquindio.proyectofinalhotelfx.Controladores to javafx.fxml;
+    // Abre paquetes a javafx.fxml
     opens co.edu.uniquindio.proyectofinalhotelfx.Controladores to javafx.fxml;
+    opens co.edu.uniquindio.proyectofinalhotelfx.Controladores.ControladoresCliente to javafx.fxml; // <--- FALTA ESTA LÍNEA
+
+    // Exporta paquetes
+    exports co.edu.uniquindio.proyectofinalhotelfx;
+    exports co.edu.uniquindio.proyectofinalhotelfx.Controladores;
+    exports co.edu.uniquindio.proyectofinalhotelfx.Controladores.ControladoresCliente;
 }
+
