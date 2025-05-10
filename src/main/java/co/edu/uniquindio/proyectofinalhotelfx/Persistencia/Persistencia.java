7e6541dbb6541a34c30062b4a38b7d31ca9ed42d
ShanT237 <santiago.rodriguezt@uqvirtual.edu.co>
2025-05-09 21:39:08 -0500

    CODEV(1.5, Backend(GUI User :D ))

--- src/main/java/co/edu/uniquindio/proyectofinalhotelfx/Persistencia/Persistencia.java
+++ src/main/java/co/edu/uniquindio/proyectofinalhotelfx/Persistencia/Persistencia.java
@@ -3,32 +3,36 @@ package co.edu.uniquindio.proyectofinalhotelfx.Persistencia;
 import java.io.*;
 
 public class Persistencia {
+
     public static void serializarObjeto(String ruta, Object objeto) throws IOException {
-        ObjectOutputStream oos = new ObjectOutputStream(new FileOutputStream(ruta));
+        File archivo = new File(ruta);
+        File carpeta = archivo.getParentFile();
+
+        if (carpeta != null && !carpeta.exists()) {
+            carpeta.mkdirs();
+        }
+
+        ObjectOutputStream oos = new ObjectOutputStream(new FileOutputStream(archivo));
         oos.writeObject(objeto);
         oos.close();
     }
 
-
     /**
-     * Metodo que permite deserializar un objeto de un archivo en la ruta especificada
+     * Método que permite deserializar un objeto de un archivo en la ruta especificada
      * @return Objeto deserializado
      * @throws Exception
      */
-    public static Object deserializarObjeto(String ruta) throws Exception{
+    public static Object deserializarObjeto(String ruta) throws Exception {
+        File archivo = new File(ruta);
 
-
-        if(!new File(ruta).exists()){
+        if (!archivo.exists()) {
             return null;
         }
 
-
-        ObjectInputStream ois = new ObjectInputStream(new FileInputStream(ruta));
+        ObjectInputStream ois = new ObjectInputStream(new FileInputStream(archivo));
         Object objeto = ois.readObject();
         ois.close();
 
-
         return objeto;
     }
-
 }
