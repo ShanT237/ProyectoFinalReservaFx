db1c3a0315870313235ed5afec5ef3599e4ce405
ShanT237 <santiago.rodriguezt@uqvirtual.edu.co>
2025-05-20 17:04:48 -0500

    CODEV1.8(MOREE CODE :D)

--- src/main/java/co/edu/uniquindio/proyectofinalhotelfx/Controladores/ControladoresAdm/GestionOfertasAdmin.java
+++ src/main/java/co/edu/uniquindio/proyectofinalhotelfx/Controladores/ControladoresAdm/GestionOfertasAdmin.java
@@ -1,13 +1,24 @@
 package co.edu.uniquindio.proyectofinalhotelfx.Controladores.ControladoresAdm;
 
+import java.io.File;
+import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
+
+import co.edu.uniquindio.proyectofinalhotelfx.Controladores.ControladorPrincipal;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.CheckBox;
 import javafx.scene.control.ComboBox;
+import javafx.scene.image.Image;
 import javafx.scene.layout.FlowPane;
+import javafx.stage.Modality;
+import javafx.stage.Stage;
 
 public class GestionOfertasAdmin {
 
@@ -24,13 +35,18 @@ public class GestionOfertasAdmin {
     private ComboBox<?> boxCiudad;
 
     @FXML
-    private Button btnCrearOferta;
-
-    @FXML
     private CheckBox esGlobal;
 
     @FXML
     private FlowPane flowPaneOferfas;
+
+    private ControladorPrincipal controladorPrincipal = ControladorPrincipal.getInstancia();
+
+
+    @FXML
+    void initialize() {
+    }
+
 
     @FXML
     void buscarOferta(ActionEvent event) {
@@ -38,13 +54,33 @@ public class GestionOfertasAdmin {
     }
 
     @FXML
-    void initialize() {
-        assert boxAlojamiento != null : "fx:id=\"boxAlojamiento\" was not injected: check your FXML file 'GestionOfertasAdmin.fxml'.";
-        assert boxCiudad != null : "fx:id=\"boxCiudad\" was not injected: check your FXML file 'GestionOfertasAdmin.fxml'.";
-        assert btnCrearOferta != null : "fx:id=\"btnCrearOferta\" was not injected: check your FXML file 'GestionOfertasAdmin.fxml'.";
-        assert esGlobal != null : "fx:id=\"esGlobal\" was not injected: check your FXML file 'GestionOfertasAdmin.fxml'.";
-        assert flowPaneOferfas != null : "fx:id=\"flowPaneOferfas\" was not injected: check your FXML file 'GestionOfertasAdmin.fxml'.";
+    void btnCrearOferta(ActionEvent event) {
+        mostrarVentana(event, "/co/edu/uniquindio/proyectofinalhotelfx/FXMLDW(ADMIN)/RegistrarOfertas.fxml", "Registrar Oferta");
 
+    }
+
+    @FXML
+    private void mostrarVentana(ActionEvent event, String ruta, String titulo) {
+        try {
+            FXMLLoader loader = new FXMLLoader(getClass().getResource(ruta));
+            Parent root = loader.load();
+
+            Stage stage = new Stage();
+            File archivoImagen = new File("Img/ImagenesApp/icon.png");
+            Image icono = new Image(archivoImagen.toURI().toString());
+            stage.setResizable(false);
+            stage.setTitle(titulo);
+
+            Stage parentStage = (Stage) ((Node) event.getSource()).getScene().getWindow();
+            stage.initOwner(parentStage);
+            stage.initModality(Modality.WINDOW_MODAL);
+
+            stage.setScene(new Scene(root));
+            stage.showAndWait();
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 
 }
