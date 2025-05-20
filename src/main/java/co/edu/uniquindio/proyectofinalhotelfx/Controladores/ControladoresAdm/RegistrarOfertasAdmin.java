785c5100653e7af58edfcad06fcf2402c7cecdc5
ShanT237 <santiago.rodriguezt@uqvirtual.edu.co>
2025-05-20 17:31:51 -0500

    CODEV1.8(MOREE CODE :D)

--- src/main/java/co/edu/uniquindio/proyectofinalhotelfx/Controladores/ControladoresAdm/RegistrarOfertasAdmin.java
+++ src/main/java/co/edu/uniquindio/proyectofinalhotelfx/Controladores/ControladoresAdm/RegistrarOfertasAdmin.java
@@ -1,12 +1,64 @@
 package co.edu.uniquindio.proyectofinalhotelfx.Controladores.ControladoresAdm;
 
+import java.net.URL;
+import java.util.ResourceBundle;
+
+import co.edu.uniquindio.proyectofinalhotelfx.Modelo.Enums.Ciudad;
+import co.edu.uniquindio.proyectofinalhotelfx.Modelo.Enums.OfertaTipo;
+import co.edu.uniquindio.proyectofinalhotelfx.Modelo.Enums.TipoAlojamiento;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.scene.control.ComboBox;
 import javafx.scene.control.DatePicker;
+import javafx.scene.control.Slider;
 import javafx.scene.control.TextField;
-import javafx.stage.Stage;
 
 public class RegistrarOfertasAdmin {
 
+    @FXML
+    private ResourceBundle resources;
+
+    @FXML
+    private URL location;
+
+    @FXML
+    private ComboBox<TipoAlojamiento> alojamientoBox;
+
+    @FXML
+    private ComboBox<Ciudad> ciudadBox;
+
+    @FXML
+    private TextField descripcionField;
+
+    @FXML
+    private ComboBox<Double> descuentoField;
+
+    @FXML
+    private DatePicker fechaFin;
+
+    @FXML
+    private DatePicker fechaInicio;
+
+    @FXML
+    private TextField idField;
+
+    @FXML
+    private TextField nombreField;
+
+    @FXML
+    private Slider sliderNochesMin;
+
+    @FXML
+    private ComboBox<OfertaTipo> tipoOferta;
+
+    @FXML
+    void guardarOferta(ActionEvent event) {
+
+    }
+
+    @FXML
+    void initialize() {
+
+    }
 
 }
