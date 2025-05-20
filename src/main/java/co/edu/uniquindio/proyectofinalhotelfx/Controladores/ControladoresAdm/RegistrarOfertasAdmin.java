e709aa24d0bdc673525f3a03cd593984f7eca78c
ShanT237 <santiago.rodriguezt@uqvirtual.edu.co>
2025-05-20 17:27:45 -0500

    CODEV1.8(MOREE CODE :D)

--- src/main/java/co/edu/uniquindio/proyectofinalhotelfx/Controladores/ControladoresAdm/RegistrarOfertasAdmin.java
+++ src/main/java/co/edu/uniquindio/proyectofinalhotelfx/Controladores/ControladoresAdm/RegistrarOfertasAdmin.java
@@ -8,36 +8,5 @@ import javafx.stage.Stage;
 
 public class RegistrarOfertasAdmin {
 
-    @FXML
-    private TextField txtCodigo, txtDescripcion, txtPorcentaje;
 
-    @FXML
-    private DatePicker dpFechaInicio, dpFechaFin;
-
-    @FXML
-    public void guardarOferta(ActionEvent event) {
-        // Validación básica
-        if (txtCodigo.getText().isBlank() || txtDescripcion.getText().isBlank() || txtPorcentaje.getText().isBlank()
-                || dpFechaInicio.getValue() == null || dpFechaFin.getValue() == null) {
-            System.out.println("Por favor, completa todos los campos.");
-            return;
-        }
-
-        System.out.println("Guardando oferta...");
-        System.out.println("Código: " + txtCodigo.getText());
-        System.out.println("Descripción: " + txtDescripcion.getText());
-        System.out.println("Descuento: " + txtPorcentaje.getText());
-        System.out.println("Fecha inicio: " + dpFechaInicio.getValue());
-        System.out.println("Fecha fin: " + dpFechaFin.getValue());
-
-        // Cierra la ventana después de guardar
-        Stage stage = (Stage) txtCodigo.getScene().getWindow();
-        stage.close();
-    }
-
-    @FXML
-    public void cancelarRegistro(ActionEvent event) {
-        Stage stage = (Stage) txtCodigo.getScene().getWindow();
-        stage.close();
-    }
 }
