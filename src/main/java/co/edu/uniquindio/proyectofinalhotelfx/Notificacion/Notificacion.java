78877db611f74da09a3d091920cf779e9d9c4425
thelegendsxv <josem.bedoyam@uqvirtual.edu.co>
2025-05-21 20:47:29 -0500

    corregirNotificar

--- src/main/java/co/edu/uniquindio/proyectofinalhotelfx/Notificacion/Notificacion.java
+++ src/main/java/co/edu/uniquindio/proyectofinalhotelfx/Notificacion/Notificacion.java
@@ -3,6 +3,7 @@ package co.edu.uniquindio.proyectofinalhotelfx.Notificacion;
 import java.time.LocalDateTime;
 import java.util.UUID;
 
+import jakarta.activation.DataSource;
 import org.simplejavamail.api.email.Email;
 import org.simplejavamail.api.mailer.Mailer;
 import org.simplejavamail.api.mailer.config.TransportStrategy;
@@ -36,5 +37,31 @@ public class Notificacion {
             e.printStackTrace();
         }
     }
+
+    public static void enviarNotificacionImagen(String correo, String s, String s1, DataSource ds) {
+        Email email = EmailBuilder.startingBlank()
+                .from("proyectofinalprogramacion053@gmail.com")
+                .to(correo)
+                .withSubject(s)
+                .withPlainText(s1)
+                .withEmbeddedImage("qr-code", ds)
+                .buildEmail();
+
+
+        try (Mailer mailer = MailerBuilder
+                .withSMTPServer("smtp.gmail.com", 587, "proyectofinalprogramacion053@gmail.com", "gohp ilel gmil kfpb")
+                .withTransportStrategy(TransportStrategy.SMTP_TLS)
+                .withDebugLogging(true)
+                .buildMailer()) {
+
+
+            mailer.sendMail(email);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+
 }
+
 //
